d32ba46177e603e9e25eaa3d0bd1f9ca70812507
Dylan Justice <dylanjustice41@gmail.com>
2021-06-22 21:42:03 -0400

    Adding rds aurora postgres, vpc information and elastic beanstalk settings

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws",
-      version = "~> 3.27"
+      source  = "hashicorp/aws"
+      version = "3.46.0"
     }
   }
-
-  required_version = ">= 0.14.9"
 }
-
 provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
 
+locals {
+  environment = "production"
+}
 # IAM
 resource "aws_iam_service_linked_role" "elasticbeanstalk" {
   aws_service_name = "elasticbeanstalk.amazonaws.com"
@@ -21,10 +21,95 @@ resource "aws_iam_service_linked_role" "elasticbeanstalk" {
 
 # Network
 ## VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/24"
+}
+
+resource "aws_subnet" "app" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+resource "aws_subnet" "database" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+}
+resource "aws_subnet" "elb" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.3.0/24"
+}
+
+resource "aws_security_group" "db" {
+  name        = "sg-rds-${local.environment}"
+  description = "Traffic to the database"
+  vpc_id      = aws_vpc.main.id
+  ingress = {
+    description      = "App Subnet traffic to database"
+    from_port        = 443
+    to_port          = 5432
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+  egress = {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+# Database
+resource "aws_db_instance" "db" {
+  allocated_storage      = 10
+  availability_zone      = "us-east-1a"
+  db_subnet_group_name   = aws_subnet.database.name
+  engine                 = "aurora-postgresql"
+  engine_version         = 5.7
+  instance_class         = "db.t3.micro"
+  password               = var.db_password
+  publicly_accessible    = true
+  skip_final_snapshot    = true
+  username               = var.db_username
+  vpc_security_group_ids = [aws_security_group.db.id]
+  identifier             = "boots-${local.environment}-db"
+}
+
+resource "aws_db_subnet_group" "db_subnet_group" {
+  name       = "main"
+  subnet_ids = [aws_subnet.database.id]
+}
+
+# AWS S3
+## Frontend public bucket
+
+## Public Asset Bucket
+
+## Private Asset Bucket
 
 # EC2
 ## Security Group
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
 
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
 
 # Elastic Beanstalk
 resource "aws_elastic_beanstalk_application" "api" {
@@ -89,7 +174,7 @@ resource "aws_elastic_beanstalk_environment" "api_environment" {
   setting {
     namespace = "aws:ec2:instances"
     name      = "InstanceTypes"
-    value     = ["t3a.medium"]
+    value     = "t3a.medium"
   }
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
@@ -107,16 +192,104 @@ resource "aws_elastic_beanstalk_environment" "api_environment" {
     value     = "100"
   }
 
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "vpc"
+    value     = aws_vpc.main.id
+  }
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "Subnets"
+    value     = aws_subnet.app.id
+  }
+  setting {
+    namespace = "aws:autoscaling:updatepolicy:rollingupdate"
+    name      = "RollingUpdateType"
+    value     = "Health"
+  }
+  setting {
+    namespace = "aws:autoscaling:updatepolicy:rollingupdate"
+    name      = "RollingUpdateEnabled"
+    value     = "true"
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:application"
+    name      = "Application Healthcheck URL"
+    value     = "/"
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "environment"
+    value     = local.environment
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "ConnectionStrings__GbContext"
+    value     = "Host=${aws_db_instance.endpoint};Port=${aws_db_instance.port};Database=${var.default_database_name};Username=${var.db_username};Password=${var.db_password}"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:cloudwatch:logs"
+    name      = "StreamLogs"
+    value     = true
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:cloudwatch:logs"
+    name      = "DeleteOnTerminate"
+    value     = true
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:cloudwatch:logs"
+    name      = "RetentionInDays"
+    value     = 7
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:cloudwatch:logs:health"
+    name      = "HealthStreamingEnabled"
+    value     = true
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:cloudwatch:logs:health"
+    name      = "DeleteOnTerminate"
+    value     = true
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:cloudwatch:logs:health"
+    name      = "RetentionInDays"
+    value     = 7
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:command"
+    name      = "DeploymentPolicy"
+    value     = "Rolling"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "EnvironmentType"
+    value     = "LoadBalanced"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "ServiceRole"
+    value     = aws_iam_service_linked_role.elasticbeanstalk.name
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "LoadBalancerType"
+    value     = "application"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:healthreporting"
+    name      = "SystemType"
+    value     = "enhanced"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:hostmanager"
+    name      = "LogPublicationControl"
+    value     = "true"
+  }
+
 }
 
-# RDS
 
-# AWS S3
-## Frontend public bucket
-
-## Public Asset Bucket
-
-## Private Asset Bucket
-
-
-# Variables
